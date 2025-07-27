88f5805cff7317b63739965d24aa1e3cfa0df5f1
LucasRatouit <ratouitlucaspro@gmail.com>
2025-07-27 20:08:56 +0200

    feat: integrate react-router for navigation and add theme provider
    - Added react-router dependencies and configured routing in the application.
    - Created a ThemeProvider component to manage theme state and persist it in local storage.
    - Updated App component to include navigation links for login and registration.
    - Created placeholder pages for login and registration.
    - Defined routes in a new routes configuration file.
    - Updated CSS variables for theming and added utility classes for JobLog styling.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,42 @@
-import { Button } from "./components/ui/button";
+import { Link } from "react-router";
 
 function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <h1>Vite + React + Shadcn</h1>
-      <Button>Button Test</Button>
+    <div
+      className="h-screen pt-52 flex flex-col items-center text-center
+                    bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-100
+                    dark:from-gray-900 dark:via-gray-800 dark:to-gray-900
+                    "
+    >
+      <div className="max-w-[800px]">
+        <h1
+          className="flex flex-col items-center
+      text-joblog-400 text-7xl font-bold mb-10"
+        >
+          Gérez vos candidatures{" "}
+          <span className="text-joblog-600">intelligemment</span>
+        </h1>
+        <p className="mb-10 text-xl font-sans">
+          JobLog vous aide à organiser, suivre et optimiser vos recherches
+          d'emploi. Gardez une trace de toutes vos candidatures en un seul
+          endroit.
+        </p>
+        <div className="mt-2 font-medium flex justify-center space-x-6">
+          <Link
+            to="/auth/login"
+            className="text-blue-500 underline underline-offset-4"
+          >
+            Déjà un compte ? Se connecter
+          </Link>
+          <span className="text-gray-500">|</span>
+          <Link
+            to="/auth/register"
+            className="text-blue-500 underline underline-offset-4"
+          >
+            Nouveau ? Créer un compte
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/dashboard",
+    element: <p>Dashboard</p>,
+  },
+  {
+    path: "auth",
+    // Component: () => <p>Auth Page</p>,
+    children: [
+      { path: "login", element: <p>Login Page</p> },
+      { path: "register", element: <p>Register Page</p> },
+    ],
+  },
+  {
+    path: "*",
+    element: <p>Not Found</p>,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider defaultTheme="system" storageKey="joblog-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
+);
