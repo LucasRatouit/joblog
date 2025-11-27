aa0670dc19921ebc9acb39402a31aa1d411207e8
Ratouit Lucas <ratouitlucaspro@gmail.com>
2025-11-27 21:56:29 +0100

    feat: ajouter des dates de candidature, d'entretien et de relance dans le composant Jobs

--- front/src/components/jobs.tsx
+++ front/src/components/jobs.tsx
@@ -44,21 +44,54 @@ const Jobs = (props: {
               )}
             </div>
             <div className="z-10 grid gap-3 grid-cols-[repeat(auto-fill,minmax(200px,1fr))]">
-              {Array.from({ length: 3 }).map((_, index) => (
-                <div
-                  key={index}
-                  className="w-full py-2 flex gap-x-3 flex-wrap items-center rounded-lg relative"
-                >
-                  <div className="-z-20 bg-green-400 opacity-15 w-full h-full rounded-lg absolute" />
-                  <Calendar className="text-green-400 w-5 h-5 ml-2" />
-                  <div className="flex flex-col">
-                    <p className="text-gray-400 text-nowrap text-[10px]">
-                      Date de l'entretien
-                    </p>
-                    <span className="text-green-400 text-sm">12/12/2023</span>
-                  </div>
+              <div className="w-full py-2 flex gap-x-3 flex-wrap items-center rounded-lg relative">
+                <div className="-z-20 bg-green-400 opacity-15 w-full h-full rounded-lg absolute" />
+                <Calendar className="text-green-400 w-5 h-5 ml-2" />
+                <div className="flex flex-col">
+                  <p className="text-gray-400 text-nowrap text-[10px]">
+                    Date de candidature
+                  </p>
+                  <span className="text-green-400 text-sm">
+                    {job.candidacyDate
+                      ? job.candidacyDate.toDateString()
+                      : "xxx"}
+                  </span>
                 </div>
-              ))}
+              </div>
+              <div className="w-full py-2 flex gap-x-3 flex-wrap items-center rounded-lg relative">
+                <div className="-z-20 bg-green-400 opacity-15 w-full h-full rounded-lg absolute" />
+                <Calendar className="text-green-400 w-5 h-5 ml-2" />
+                <div className="flex flex-col">
+                  <p className="text-gray-400 text-nowrap text-[10px]">
+                    Date de l'entretien
+                  </p>
+                  <span className="text-green-400 text-sm">
+                    {job.interviewDate
+                      ? new Date(job.interviewDate).toLocaleString("fr-FR", {
+                          day: "2-digit",
+                          month: "2-digit",
+                          year: "numeric",
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })
+                      : "xxx"}
+                  </span>
+                </div>
+              </div>
+              <div className="w-full py-2 flex gap-x-3 flex-wrap items-center rounded-lg relative">
+                <div className="-z-20 bg-green-400 opacity-15 w-full h-full rounded-lg absolute" />
+                <Calendar className="text-green-400 w-5 h-5 ml-2" />
+                <div className="flex flex-col">
+                  <p className="text-gray-400 text-nowrap text-[10px]">
+                    Date de relance
+                  </p>
+                  <span className="text-green-400 text-sm">
+                    {job.followUpDate
+                      ? new Date(job.followUpDate).toDateString()
+                      : "xxx"}
+                  </span>
+                </div>
+              </div>
             </div>
             {job.description && (
               <p className="bg-zinc-800 text-gray-400 p-3 rounded-lg">
